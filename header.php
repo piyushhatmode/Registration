e68c3e30efcc5b630a24c3e845dea314fa3933b8
Piyush Hatmode <piyush.hatmode.pph@gmail.com>
2018-08-15 05:45:09 -0700

    footer added

--- header.php
+++ header.php
@@ -6,7 +6,10 @@ session_start();
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="style.css">
-    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <title>Index</title>
   </head>
   <body>
     <header>
